2147b94cc0544fc3643c0a1732260d193236e2d5
Ren <111091454+RenHojoo@users.noreply.github.com>
2025-11-02 21:59:15 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/moodjournal/',  // important for GH Pages
   plugins: [react()],
 });
